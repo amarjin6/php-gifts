94a4f9287846a6dd28aa4d455b4c6eaa6139a97e
Alexander Marjin <amarjin6@gmail.com>
2022-05-17 23:57:14 +0300

    Created FileManager

---
+++ Trivial web applications/run_task1.php
@@ -0,0 +1,32 @@
+<?php
+include_once "task1.php";
+error_reporting(0);
+$fileManager = new FileManager();
+if (!array_key_exists("flag", $_COOKIE)) {
+    setcookie("flag", true);
+    $fileManager->showTop();
+    $fileManager->upload($_FILES);
+    $fileManager->display();
+    $fileManager->showBot();
+} else {
+    if (!$_COOKIE["flag"]) {
+        setcookie("flag", true);
+        $fileManager->showTop();
+        $fileManager->upload($_FILES);
+        $fileManager->display();
+        $fileManager->showBot();
+    }
+}
+if (array_key_exists('upload', $_POST)) {
+    $fileManager->showTop();
+    $fileManager->upload($_FILES);
+    $fileManager->display();
+    $fileManager->showBot();
+} elseif (array_key_exists('download', $_POST)) {
+    $fileManager->download($_POST['name']);
+} elseif (array_key_exists('delete', $_POST)) {
+    $fileManager->showTop();
+    $fileManager->delete($_POST['name']);
+    $fileManager->display();
+    $fileManager->showBot();
+}
---
+++ Trivial web applications/task1.php
@@ -0,0 +1,99 @@
+<?php
+
+class FileManager
+{
+
+    public function display()
+    {
+        $dir = '';
+        $files = scandir($dir, 1);
+        if (count(array($files)) == 2) {
+            echo "<h2>Empty</h2>";
+        } else {
+            for ($i = 0; $i < count(array($files)) - 2; $i++) {
+                echo "<br>";
+                echo "<h3 style='display: inline-block;'>";
+                echo $i + 1 . ") " . $files[$i];
+                echo "</h3>";
+                echo "<form method='post' style='display: inline-block; margin-left: 10px;'>";
+                echo "<input type='hidden' name='name' value='" . $i . "'/>";
+                echo "<input type='submit' name='download' value='Download'/>";
+                echo "</form>";
+                echo "<form method='post' style='display: inline-block; margin-left: 10px'>";
+                echo "<input type='hidden' name='name' value='" . $i . "'/>";
+                echo "<input type='submit' name='delete' value='Delete'/>";
+                echo "</form>";
+                echo "</div>";
+            }
+        }
+    }
+
+    public function upload($files)
+    {
+        if ($files && $files["filename"]["error"] == UPLOAD_ERR_OK) {
+            $name = "server/" . $files["filename"]["name"];
+            move_uploaded_file($files["filename"]["tmp_name"], $name);
+            echo "File uploaded!<br>";
+        }
+
+    }
+
+    public function download($name)
+    {
+        $dir = 'server';
+        $files = scandir($dir, 1);
+        $file = $files[$name];
+        $file_path = 'server/' . $file;
+        $finfo = finfo_open(FILEINFO_MIME_TYPE);
+        $mime = finfo_file($finfo, $file_path);
+        header('Content-Disposition: attachment; filename="' . basename($file_path) . '"');
+        header('Content-Type: ' . $mime);
+        header('Content-Length: ' . filesize($file_path));
+        header('Connection: close');
+        echo file_get_contents($file_path);
+    }
+
+    public function delete($name)
+    {
+        $dir = 'server';
+        $files = scandir($dir, 1);
+        $file = $files[$name];
+        $filepath = "server/" . $file;
+        unlink($filepath);
+    }
+
+    public function showTop()
+    {
+        echo "<!DOCTYPE html>
+                <html>
+                <head>
+                    <title>Task 1</title>
+                    <meta charset='utf-8' />
+                </head>
+                <body>";
+        echo "<form method='post' enctype='multipart/form-data'>
+          Choose file:
+          <br>
+          <input type='file' name='filename' size='10' />
+          <br>
+          <input type='submit' name='upload' value='Upload' style='  border: none;
+            background-color: dodgerblue;
+            color: white;
+            margin-top: 5px;
+            margin-left: 60px;
+            padding: 5px 20px;
+            text-align: center;
+            text-decoration: none;
+            font-size: 16px;
+            cursor: pointer'/>
+          </form>";
+    }
+
+    public function showBot()
+    {
+        echo "</body>
+            </html>";
+    }
+
+
+}
